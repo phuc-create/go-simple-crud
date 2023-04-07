b938b7b13f8c021f20ce803a73e5b80ea80913fa
ext-phuc-nguyen <ext-phuc.nguyen@spdigital.sg>
2023-04-07 16:27:53 +0700

    add go chi

--- main.go
+++ main.go
@@ -16,34 +16,11 @@ const (
 	DELETE = "DELETE"
 )
 
-type ObjSimple interface {
-	Checker() bool
-}
-type Bol struct {
-	B bool
-}
-
-func (b Bol) Checker() bool {
-	return false
-}
-
 func main() {
-
-	// var idea ObjSimple
-	// if idea != nil {
-	// 	fmt.Println("not null")
-	// } else {
-	// 	fmt.Println(idea)
-	// }
-
 	router := mux.NewRouter()
 	router.HandleFunc("/api/user/find", apis.FindUser).Methods(GET)
 	router.HandleFunc("/api/user/users", apis.GetUsers).Methods(GET)
 
-	// err := http.ListenAndServe(":5050", router)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	fmt.Printf("Start server at port 5050\n")
+	fmt.Printf("Start server at port http://localhost:5050\n")
 	log.Fatal(http.ListenAndServe(":5050", router))
 }
