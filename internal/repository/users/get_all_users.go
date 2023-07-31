c13f793a042030a15715b9c2603d6123d9374488
ext-phuc-nguyen <TNHP921C@spgroup.com.sg>
2023-07-31 22:42:11 +0700

    Custom response

--- internal/repository/users/get_all_users.go
+++ internal/repository/users/get_all_users.go
@@ -2,9 +2,7 @@ package users
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/friendsofgo/errors"
 	"github.com/phuc-create/go-simple-crud/internal/repository/orm"
 	"github.com/phuc-create/go-simple-crud/models"
 	"github.com/phuc-create/go-simple-crud/pkgErrors"
@@ -15,18 +13,17 @@ func (i implement) GetAllUsers(ctx context.Context) ([]models.User, error) {
 	boil.DebugMode = true
 
 	users, err := orm.UserAccounts().All(ctx, i.db)
-	fmt.Println(users, err)
 	if err != nil {
 		return nil, err
 	}
-	if len(users) < 1 {
-		return nil, errors.WithStack(pkgErrors.ErrCouldNotFindAnyUser)
-	}
-
 	var result []models.User
 
 	for _, c := range users {
 		result = append(result, toUser(c))
+	}
+
+	if len(result) < 1 {
+		return nil, pkgErrors.ErrCouldNotFindAnyUser
 	}
 	return result, nil
 }
