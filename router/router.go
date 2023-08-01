7ac85022a3307da7a9cfd6c864fa56e22465b22e
ext-phuc-nguyen <ext-phuc.nguyen@spdigital.sg>
2023-08-01 13:26:33 +0700

    refactor folder

--- router/router.go
+++ router/router.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/phuc-create/go-simple-crud/handlers/users"
+	"github.com/phuc-create/go-simple-crud/internal/handlers/users"
 	"github.com/phuc-create/go-simple-crud/internal/repository"
 	users2 "github.com/phuc-create/go-simple-crud/internal/repository/controllers/users"
 	"log"
